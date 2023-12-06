0ade9082798216d7d6ac190e1409158fb0711df1
membrillo amargo <lopezrunco@gmail.com>
2023-12-06 11:40:25 +0530

    Backoffice home page: new actions added

--- src/pages/backoffice/BackOfficeHome/index.jsx
+++ src/pages/backoffice/BackOfficeHome/index.jsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import React from "react";
 
 import { Breadcrumbs } from "../../../components/Breadcrumbs";
-import { Title } from "../../../components/Title";
 
 import "./styles.scss";
 
@@ -25,21 +24,54 @@ export const BackOfficeHome = () => {
       >
         <section className="backoffice-home-page">
           <article className="container">
-            <Title title="¿Qué desea hacer?" />
             <div className="row">
-              <div className="col-12 col-lg-4 offset-lg-4 admin-home-card">
+              <div className="col-12 col-lg-3 mb-5 mb-lg-0 admin-home-card">
+                <h3>Remates</h3>
+                <div className="separator"></div>
                 <a href="/consignatarios/crear-remate">
-                  <i className="fas fa-plus me-2"></i> Crear remate
+                  <i className="fas fa-gavel me-2"></i> Crear remate
                 </a>
-              </div>
-              <div className="col-12 col-lg-4 offset-lg-4 admin-home-card">
                 <a href="/admin/remates">
-                  <i className="fas fa-list me-2"></i>Lista de remates
+                  <i className="fas fa-list me-2"></i>Listar remates
                 </a>
               </div>
-              <div className="col-12 col-lg-4 offset-lg-4 admin-home-card">
+              <div className="col-12 col-lg-3 mb-5 mb-lg-0 admin-home-card">
+                <h3>Artículos</h3>
+                <div className="separator"></div>
+                <a href="/admin/articulos/crear">
+                  <i className="fas fa-file me-2"></i> Crear artículo
+                </a>
+                <a href="/admin/articulos/listar">
+                  <i className="fas fa-list me-2"></i>Listar artículos
+                </a>
+                <a href="/admin/articulos/categorias/crear">
+                  <i className="fas fa-folder-plus me-2"></i>Crear categoría
+                </a>
+                <a href="/admin/articulos/categorias/listar">
+                  <i className="fas fa-list me-2"></i>Listar categorías
+                </a>
+                <a href="/admin/articulos/etiquetas/listar">
+                  <i className="fas fa-tags me-2"></i>Listar etiquetas
+                </a>
+              </div>
+              <div className="col-12 col-lg-3 mb-5 mb-lg-0 admin-home-card">
+                <h3>Publicidad</h3>
+                <div className="separator"></div>
+                <a href="/admin/publicidad/crear">
+                  <i className="fas fa-bullhorn me-2"></i> Crear publicidad
+                </a>
+                <a href="/admin/publicidad/listar">
+                  <i className="fas fa-list me-2"></i>Listar publicidades
+                </a>
+                <a href="/admin/publicidad/ayuda">
+                  <i className="fas fa-info-circle me-2"></i>Ayuda
+                </a>
+              </div>
+              <div className="col-12 col-lg-3 mb-5 mb-lg-0 admin-home-card">
+                <h3>Usuarios</h3>
+                <div className="separator"></div>
                 <a href="/admin/usuarios">
-                  <i className="fas fa-users me-2"></i>Usuarios activos
+                  <i className="fas fa-users me-2"></i>Listar usuarios
                 </a>
               </div>
             </div>
