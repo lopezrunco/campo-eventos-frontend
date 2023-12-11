b4fb91ccbb2ed88b28d1508f390450710195c47e
membrillo amargo <lopezrunco@gmail.com>
2023-12-11 10:30:43 +0530

    My posts Card component styles

--- src/pages/blog/MyPosts/components/PostByUserCard/index.jsx
+++ src/pages/blog/MyPosts/components/PostByUserCard/index.jsx
@@ -1,27 +1,51 @@
 import { useNavigate } from "react-router-dom";
 
+import imgUrl from "../../../../../assets/no-img.jpg";
+
 import { getDate } from "../../../../../utils/get-date";
+
+import "./styles.scss";
 
 export const PostByUserCard = ({ post }) => {
   const navigate = useNavigate();
 
   return (
     <div className="col-12">
-      <div className="my-event-card">
+      <div className="my-post-card">
         <div className="row">
           <div className="col-lg-9">
-            <h2>{post.title}</h2>
-            <h3>Categoría: {post.category}</h3>
-            <p>{post.headline}</p>
-            {post.tags.map((tag, i) => {
-              return <small key={i}>{tag}</small>;
-            })}
-            <p>Creado: {getDate(post.createdAt)}</p>
-            <p>Modificado: {getDate(post.updatedAt)}</p>
-            <h4>Iconos de borrar y editar</h4>
+            <div className="content">
+              <h3>{post.title}</h3>
+              <p className="date">Creado: {getDate(post.createdAt)}</p>
+              <p>{post.headline}</p>
+              <div className="tags">
+                {post.tags.map((tag, i) => {
+                  return (
+                    <span className="tag gray" key={i}>
+                      {tag}
+                    </span>
+                  );
+                })}
+              </div>
+              <a
+                className="button view-more mb-0"
+                onClick={() =>
+                  navigate(`/autor/articulos/mis-articulos${post.id}`)
+                }
+              >
+                <i className="fas fa-chevron-right me-2"></i> Ver más / Editar
+              </a>
+            </div>
           </div>
           <div className="col-lg-3">
-            <img width="100%" src={post.picture} alt={post.title} />
+            {post.category && (
+              <span className="post-type-tag">{post.category}</span>
+            )}
+            {post.picture ? (
+              <img src={post.picture} width="100%" />
+            ) : (
+              <img src={imgUrl} width="100%" />
+            )}
           </div>
         </div>
       </div>
