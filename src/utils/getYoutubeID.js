b956dda3be35eefc901ecc8cd28c2b0b059bf9d5
lopezrunco <lopezrunco@gmail.com>
2024-05-27 15:01:15 +0530

    fix: Update getYoutubeId util regex to allow new Youtube live video link.

--- src/utils/getYoutubeID.js
+++ src/utils/getYoutubeID.js
@@ -11,7 +11,7 @@ export const getYoutubeId = (code) => {
 
     function getYoutubeUrlId(url) {
         const regExp =
-            /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|live\/|watch\?v=|\&v=)([^#\&\?]*).*/;
         const match = url.match(regExp);
         return match && match[2].length === 11 ? match[2] : undefined;
     }
